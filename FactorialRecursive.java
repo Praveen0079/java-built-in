fc90789c48c95e24e9aa306425f177267dc7749e
Praveen Sharma <96952903+Praveen0079@users.noreply.github.com>
2025-03-08 14:23:24 +0530

    [Praveen] ADD: Factorial using Recursive function

--- FactorialRecursive.java
+++ FactorialRecursive.java
@@ -4,7 +4,7 @@ public class FactorialRecursive {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Get user input
+        // Get user input 
         System.out.print("Enter a number to calculate its factorial: ");
         int num = scanner.nextInt();
 
